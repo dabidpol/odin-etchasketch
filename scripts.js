4e5c4b85a793c4db2dfc6d6a1a0e152a0e94ac0a
dabidpol <dcabatingan@gmail.com>
2022-09-23 18:30:26 +0800

    updated with logic, but still not finished

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
 createGrid = () => {
+    
     for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('div');
@@ -10,5 +11,40 @@ createGrid = () => {
         container.appendChild(div); 
     }
     };
+
+function reset(){
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+      }
+}
+
+
+function apply(){
+    let entered = parseInt(getEnteredNumber());
+    let size = entered * entered;
+    container.setAttribute('style', `grid-template-columns: repeat(${entered}, 2fr); grid-template-rows: repeat(${entered}, 2fr);`);
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('div');
+            div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'blue';
+        })
+        container.appendChild(div); 
+    }
+}
+
+
+
+function getEnteredNumber(){
+    const n = document.getElementById("enteredNumber").value;
+    console.log(n)
+    return n
+}
+
+// if(entered <= 25){
+//     size = entered * entered
+// }else{
+//     console.log("enter a smaller number")
+// }
 
 createGrid();
